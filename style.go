fcfdc663236db5a762ef66a5fd92ebb48d5354b9
Maas Lalani <maas@lalani.dev>
2021-12-21 17:11:53 -0500

    undo

--- style.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/charmbracelet/lipgloss"
-
-var (
-	activeStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#E8B4BC"))
-	faintStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("#808080"))
-)
