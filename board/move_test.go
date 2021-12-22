9a0d34e37d76d623cd1aac96033012b0ce273301
Maas Lalani <maas@lalani.dev>
2021-12-22 11:53:22 -0500

    move tests

--- board/move_test.go
+++ board/move_test.go
@@ -8,11 +8,25 @@ import (
 
 func TestMove(t *testing.T) {
 	initial := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-	expected := "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1"
-	b, _ := FromFen(initial)
-	b.Move(Move{E2, E4})
+	tests := []struct {
+		board    string
+		moves    []Move
+		expected string
+	}{
+		{initial, []Move{{E2, E4}}, "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1"},
+		{initial, []Move{{A7, A6}}, "rnbqkbnr/1ppppppp/p7/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"},
+		{initial, []Move{{G1, F3}}, "rnbqkbnr/pppppppp/8/8/8/5N2/PPPPPPPP/RNBQKB1R w KQkq - 0 1"},
+		{initial, []Move{{E2, E4}, {C7, C5}, {G1, F3}}, "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 0 1"},
+	}
 
-	if b.ToFen() != expected {
-		t.Errorf("Expected %s, got %s", expected, b.ToFen())
+	for _, tc := range tests {
+		b, _ := FromFen(tc.board)
+		for _, move := range tc.moves {
+			b.Move(move)
+		}
+		if b.ToFen() != tc.expected {
+			t.Errorf("\nwant %s\ngot  %s", tc.expected, b.ToFen())
+			// t.Log(b)
+		}
 	}
 }
