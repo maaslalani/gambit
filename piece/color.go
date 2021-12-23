528166050aa03ada49796ea9b3c80732e5794c14
Maas Lalani <maas@lalani.dev>
2021-12-22 23:20:02 -0500

    clean up pieces

--- piece/color.go
+++
@@ -1,9 +0,0 @@
-package piece
-
-type Color string
-
-const (
-	White   Color = "w"
-	Black   Color = "b"
-	NoColor Color = " "
-)
--- piece/piece.go
+++ piece/piece.go
@@ -1,21 +1,86 @@
 package piece
 
 import (
+	"strings"
+
 	"github.com/charmbracelet/lipgloss"
 	"github.com/maaslalani/gambit/style"
 )
 
+// Color of pieces
+type Color string
+
+const (
+	White   Color = "w"
+	Black   Color = "b"
+	NoColor Color = ""
+)
+
+// Types of pieces
+type Type string
+
+const (
+	Pawn   Type = "P"
+	Knight Type = "N"
+	Bishop Type = "B"
+	Rook   Type = "R"
+	Queen  Type = "Q"
+	King   Type = "K"
+	NoType Type = ""
+)
+
+func (t Type) String() string {
+	return string(t)
+}
+
+// Pieces
 type Piece struct {
 	Type  Type
 	Color Color
+}
+
+func (p Piece) String() string {
+	return Display[p.Type]
 }
 
 func Empty() Piece {
 	return Piece{NoType, NoColor}
 }
 
-func (p Piece) String() string {
-	return Display[p.Type]
+var Display = map[Type]string{
+	Bishop: "♝",
+	King:   "♚",
+	Knight: "♞",
+	Pawn:   "♟",
+	Queen:  "♛",
+	Rook:   "♜",
+	NoType: " ",
+}
+
+// ToFen converts a piece into its FEN representation
+// i.e. White Knight -> "N"
+//      Black Bishop -> "b"
+// if a piece is empty, it returns an empty string
+func (p Piece) ToFen() string {
+	t := string(p.Type)
+	if p.Color == Black {
+		return strings.ToLower(t)
+	}
+	return t
+}
+
+// FromFen converts a FEN representation of a piece into a
+// piece. Reverses ToFen.
+func FromFen(fen string) Piece {
+	u := strings.ToUpper(fen)
+	t := Type(u)
+	var c Color
+	if u == fen {
+		c = White
+	} else {
+		c = Black
+	}
+	return Piece{Type: t, Color: c}
 }
 
 func (p Piece) Style() lipgloss.Style {
@@ -26,23 +91,20 @@ func (p Piece) Style() lipgloss.Style {
 	}
 }
 
-var (
-	BW = Piece{Bishop, White}
-	KW = Piece{King, White}
-	NW = Piece{Knight, White}
-	PW = Piece{Pawn, White}
-	QW = Piece{Queen, White}
-	RW = Piece{Rook, White}
-)
+var EmptyPiece = Piece{NoType, NoColor}
 
 var (
 	BB = Piece{Bishop, Black}
+	BW = Piece{Bishop, White}
 	KB = Piece{King, Black}
+	KW = Piece{King, White}
 	NB = Piece{Knight, Black}
+	NW = Piece{Knight, White}
 	PB = Piece{Pawn, Black}
+	PW = Piece{Pawn, White}
 	QB = Piece{Queen, Black}
+	QW = Piece{Queen, White}
 	RB = Piece{Rook, Black}
+	RW = Piece{Rook, White}
+	OO = Piece{NoType, NoColor}
 )
-
-var OO = Piece{NoType, NoColor}
-var EmptyPiece = Piece{NoType, NoColor}
