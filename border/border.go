197fac15de4708f4f9b5ae4092f2fa4fae9e0584
Maas Lalani <maas@lalani.dev>
2022-01-31 11:19:37 -0500

    doc: add note about flipped flipped

--- border/border.go
+++ border/border.go
@@ -29,6 +29,15 @@ func Cell(x, y int, flipped bool) string {
 	col := (x - marginLeft) / cellWidth
 	row := (y - marginTop) / cellHeight
 	if !flipped {
+		// Careful: `flipped` is a bit strange here.
+		//
+		// `flipped` represents whether the _chess board_ is flipped.
+		// Normally, the order of the rows will be from 8 (top) to 1 (bottom).
+		// If the board is flipped (i.e. black's turn) then the order of the rows
+		// would be how one would normally print of the rows: 1 (top) to 8 (bottom).
+		//
+		// In other words, flipped is flipped. This trade-off is for consistency
+		// with the other parts of the code base, namely the model.flipped property
 		row = board.LastRow - row
 	}
 	return position.ToSquare(row, col)
