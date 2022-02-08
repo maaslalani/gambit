maaslalani/gambit

commit 87dfad59be9e96b501adecd02b2ac1d8556c4a4f

feat: ability to flip board

game/game.go
@@ -26,6 +26,7 @@ type model struct {
 	pieceMoves []dt.Move
 	selected   string
 	buffer     string
+	flipped    bool
 }
 
 // InitialModel returns an initial model of the game board. It uses the
@@ -79,8 +80,17 @@ func (m model) View() string {
 	// Traverse through the rows and columns of the board and print out the
 	// pieces and empty squares. Once a piece is selected, highlight the legal
 	// moves and pieces that may be captured by the selected piece.
-	for r, row := range fen.Grid(m.board.ToFen()) {
+	var rows = fen.Grid(m.board.ToFen())
+
+	for r := board.FirstRow; r < board.Rows; r++ {
+		row := rows[r]
 		rr := board.LastRow - r
+
+		if m.flipped {
+			row = rows[board.LastRow-r]
+			rr = r
+		}
+
 		s.WriteString(Faint(fmt.Sprintf(" %d ", rr+1)) + border.Vertical)
 
 		for c, cell := range row {
@@ -130,6 +140,8 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
+		case "ctrl+f":
+			m.flipped = !m.flipped
 		case "a", "b", "c", "d", "e", "f", "g", "h":
 			m.buffer = msg.String()
 		case "1", "2", "3", "4", "5", "6", "7", "8":

board/board.go
@@ -1,8 +1,13 @@
 package board
 
 const (
+	Cols = 8
+	Rows = 8
+)
+
+const (
 	FirstCol = 0
 	FirstRow = 0
-	LastCol  = 7
-	LastRow  = 7
+	LastCol  = Cols - 1
+	LastRow  = Rows - 1
 )

commit 42046e4b62f4ec4be7bbab67dcf673ee707b3582

fix: flipped board order
Fixes: 87dfad59be9e ("feat: ability to flip board")

border/border.go
@@ -1,6 +1,7 @@
 package border
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/maaslalani/gambit/board"
@@ -28,19 +29,7 @@ const (
 func Cell(x, y int, flipped bool) string {
 	col := (x - marginLeft) / cellWidth
 	row := (y - marginTop) / cellHeight
-	if !flipped {
-		// Careful: `flipped` is a bit strange here.
-		//
-		// `flipped` represents whether the _chess board_ is flipped.
-		// Normally, the order of the rows will be from 8 (top) to 1 (bottom).
-		// If the board is flipped (i.e. black's turn) then the order of the rows
-		// would be how one would normally print of the rows: 1 (top) to 8 (bottom).
-		//
-		// In other words, flipped is flipped. This trade-off is for consistency
-		// with the other parts of the code base, namely the model.flipped property
-		row = board.LastRow - row
-	}
-	return position.ToSquare(row, col)
+	return position.ToSquare(row, col, flipped)
 }
 
 // withMarginLeft returns a string with a prepended left margin
@@ -71,6 +60,17 @@ func Bottom() string {
 }
 
 // BottomLabels returns the labels for the files
-func BottomLabels() string {
-	return withMarginLeft("  A   B   C   D   E   F   G   H\n")
+func BottomLabels(flipped bool) string {
+	labels := ""
+	for i := 0; i < board.Cols; i++ {
+		c := i
+		if flipped {
+			c = board.LastCol - i
+		}
+		labels += fmt.Sprintf("%c", c+'A')
+		if i != board.LastCol {
+			labels += withMarginLeft("")
+		}
+	}
+	return withMarginLeft(fmt.Sprintf("  %s\n", labels))
 }

game/game.go
@@ -88,8 +88,12 @@ func (m model) View() string {
 		row := rows[r]
 		rr := board.LastRow - r
 
+		// reverse the row if the board is flipped
 		if m.flipped {
 			row = rows[board.LastRow-r]
+			for i, j := 0, len(row)-1; i < j; i, j = i+1, j-1 {
+				row[i], row[j] = row[j], row[i]
+			}
 			rr = r
 		}
 
@@ -97,16 +101,17 @@ func (m model) View() string {
 
 		for c, cell := range row {
 			display := pieces.Display[cell]
+			selected := position.ToSquare(r, c, m.flipped)
 
 			// The user selected the current cell, highlight it so they know it is
 			// selected.
-			if m.selected == position.ToSquare(rr, c) {
+			if m.selected == selected {
 				display = Cyan(display)
 			}
 
 			// Show all the cells to which the piece may move. If it is an empty cell
 			// we present a coloured dot, otherwise color the capturable piece.
-			if moves.IsLegal(m.pieceMoves, position.ToSquare(rr, c)) {
+			if moves.IsLegal(m.pieceMoves, selected) {
 				if cell == "" {
 					display = "."
 				}
@@ -122,7 +127,7 @@ func (m model) View() string {
 		}
 	}
 
-	s.WriteString(border.Bottom() + Faint(border.BottomLabels()))
+	s.WriteString(border.Bottom() + Faint(border.BottomLabels(m.flipped)))
 	return s.String()
 }
 

position/position.go
@@ -29,6 +29,13 @@ func rowToRank(row int) int {
 
 // ToSquare returns the square position (e.g. a1) of a given row and column
 // (e.g. 0,0) for display or checking legal moves.
-func ToSquare(row, col int) string {
+func ToSquare(row, col int, flipped bool) string {
+	// If the board is flipped, the row and column are reversed
+	// i.e. h becomes a and 8 becomes 1
+	if flipped {
+		col = board.LastCol - col
+	} else {
+		row = board.LastRow - row
+	}
 	return colToFile(col) + strconv.Itoa(rowToRank(row))
 }

border/border_test.go
@@ -24,10 +24,6 @@ func TestTopBorder(t *testing.T) {
 			border.Bottom,
 			"   └───┴───┴───┴───┴───┴───┴───┴───┘\n",
 		},
-		{
-			border.BottomLabels,
-			"     A   B   C   D   E   F   G   H\n",
-		},
 	}
 
 	for _, test := range tests {