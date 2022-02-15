4c67fe6758b1c1f9b77a14e09cfe85463aa0d732
Maas Lalani <maas@lalani.dev>
2022-02-14 20:06:02 -0500

    refactor: server/middleware.go

--- server/middleware.go
+++ server/middleware.go
@@ -15,39 +15,47 @@ import (
 func gambitMiddleware(srv *Server) wish.Middleware {
 	return func(sh ssh.Handler) ssh.Handler {
 		lipgloss.SetColorProfile(termenv.ANSI256)
+
 		return func(s ssh.Session) {
 			_, _, active := s.Pty()
+
 			cmds := s.Command()
 			if len(cmds) < 1 || !active {
 				s.Write([]byte(help("No TTY")))
 				s.Exit(1)
 				return
 			}
+
 			password := ""
+
 			id := cmds[0]
 			if len(cmds) > 1 {
 				password = cmds[1]
 			}
-			g := srv.FindRoom(id)
-			if g == nil {
+
+			room := srv.FindRoom(id)
+			if room == nil {
 				log.Printf("room %s is created with password %q", id, password)
-				g = srv.NewRoom(id, password)
+				room = srv.NewRoom(id, password)
 			}
-			if g.password == password {
-				p, err := g.AddPlayer(s)
-				if err != nil {
-					s.Write([]byte(fmt.Sprintf("%s\n", err)))
-					s.Exit(1)
-					return
-				}
-				log.Printf("%s joined room %s [%s]", s.User(), id, s.RemoteAddr())
-				p.StartGame()
-				log.Printf("%s left room %s [%s]", s.User(), id, s.RemoteAddr())
-			} else {
+
+			if room.password != password {
 				s.Write([]byte(help("wrong password")))
 				s.Exit(1)
 				return
 			}
+
+			p, err := room.AddPlayer(s)
+			if err != nil {
+				s.Write([]byte(fmt.Sprintf("%s\n", err)))
+				s.Exit(1)
+				return
+			}
+
+			log.Printf("%s joined room %s [%s]", s.User(), id, s.RemoteAddr())
+			p.StartGame()
+			log.Printf("%s left room %s [%s]", s.User(), id, s.RemoteAddr())
+
 			sh(s)
 		}
 	}
